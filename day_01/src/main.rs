5c8c689755a6e19a3fe6ae271d8390860c2245c7
Günther Schröttner <50587085+aiai-guenz@users.noreply.github.com>
2022-12-01 20:28:36 +0100

    Remove unnecessary variables.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -6,19 +6,14 @@ fn main() {
 
     let lines: Vec<&str> = contents.split('\n').collect();
     let mut max_calories = 0;
-    let mut max_elf = 0;
     let mut current_calories = 0;
-    let mut current_elf = 1;
     let mut calories_per_elf: Vec<i32> = Vec::new();
     for line in lines {
         if line.eq("") {
             if current_calories > max_calories {
-                max_elf = current_elf;
                 max_calories = current_calories;
-                println!("Now elf {max_elf} carries the maximum {max_calories}");
             }
             calories_per_elf.push(current_calories);
-            current_elf += 1;
             current_calories = 0;
         } else {
             let calories: i32 = match line.trim().parse() {
@@ -28,7 +23,7 @@ fn main() {
             current_calories += calories;
         }
     }
-    println!("Elfe {max_elf} carries {max_calories} calories");
+    println!("Max calories: {max_calories}.");
     calories_per_elf.sort();
     let number_of_elves = calories_per_elf.len();
     let sum_top3_calories = calories_per_elf[number_of_elves - 1]
