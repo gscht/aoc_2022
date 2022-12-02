6b80712cac6fa56bdba4a92d29120c8f9cd77101
gscht <gscht@mailbox.org>
2022-12-02 20:01:30 +0100

    Cleanup

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -23,63 +23,57 @@ fn main() {
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
     let lines: Vec<&str> = contents.split('\n').collect();
-    // A, X -> Rock
-    // B, Y -> Paper
-    // C, Z, -> Scissors
     let mut points = 0;
     for line in lines {
         if line.len() == 0 {
             continue;
         }
-        let p1 = line.chars().nth(0).unwrap();
-        let p1: Symbol = match p1 {
+
+        let player1_symbol = line.chars().nth(0).unwrap();
+        let player1_symbol: Symbol = match player1_symbol {
             'A' => Symbol::Rock,
             'B' => Symbol::Paper,
             'C' => Symbol::Scissors,
-            _other => panic!("Unknown input {p1}"),
+            _other => panic!("Unknown input {player1_symbol}"),
         };
-        let p2 = line.chars().nth(2).unwrap();
-        // X need to loose, Y draw, Z win
-        let p2: Outcome = match p2 {
+
+        let player2_symbol = line.chars().nth(2).unwrap();
+        let outcome: Outcome = match player2_symbol {
             'X' => Outcome::Loose,
             'Y' => Outcome::Draw,
             'Z' => Outcome::Win,
-            _other => panic!("Unknown input {p2}"),
+            _other => panic!("Unknown input {player2_symbol}"),
         };
 
-        let p2: Symbol = match p2 {
-            Outcome::Win => get_winning_symbol(p1),
-            Outcome::Loose => get_loosing_symbol(p1),
-            Outcome::Draw => get_draw_symbol(p1),
+        // Get symbol so that outcome is fulfilled.
+        let player2_symbol: Symbol = match outcome {
+            Outcome::Win => get_winning_symbol(&player1_symbol),
+            Outcome::Loose => get_loosing_symbol(&player1_symbol),
+            Outcome::Draw => get_draw_symbol(&player1_symbol),
         };
 
         // Add score for shape
-        points += match p2 {
+        points += match player2_symbol {
             Symbol::Rock => 1,
             Symbol::Paper => 2,
             Symbol::Scissors => 3,
         };
 
         // Add score for result
-        if p1 == p2 {
+        if player1_symbol == player2_symbol {
             points += 3;
-        } else if p2 == Symbol::Rock && p1 == Symbol::Scissors {
-            //println!("Won {p1} {p2}");
+        } else if player2_symbol == Symbol::Rock && player1_symbol == Symbol::Scissors {
             points += 6;
-        } else if p2 == Symbol::Paper && p1 == Symbol::Rock {
-            //println!("Won {p1} {p2}");
+        } else if player2_symbol == Symbol::Paper && player1_symbol == Symbol::Rock {
             points += 6;
-        } else if p2 == Symbol::Scissors && p1 == Symbol::Paper {
-            //println!("Won {p1} {p2}");
+        } else if player2_symbol == Symbol::Scissors && player1_symbol == Symbol::Paper {
             points += 6;
-        } else {
-            //println!("Lost {p1} {p2}");
-        }
+        } 
     }
     println!("Your points: {points}");
 }
 
-fn get_draw_symbol(s: Symbol) -> Symbol {
+fn get_draw_symbol(s: &Symbol) -> Symbol {
     match s {
         Symbol::Rock => Symbol::Rock,
         Symbol::Paper => Symbol::Paper,
@@ -87,14 +81,14 @@ fn get_draw_symbol(s: Symbol) -> Symbol {
     }
 }
 
-fn get_winning_symbol(s: Symbol) -> Symbol {
+fn get_winning_symbol(s: &Symbol) -> Symbol {
     match s {
         Symbol::Rock => Symbol::Paper,
         Symbol::Paper => Symbol::Scissors,
         Symbol::Scissors => Symbol::Rock,
     }
 }
-fn get_loosing_symbol(s: Symbol) -> Symbol {
+fn get_loosing_symbol(s: &Symbol) -> Symbol {
     match s {
         Symbol::Rock => Symbol::Scissors,
         Symbol::Paper => Symbol::Rock,
