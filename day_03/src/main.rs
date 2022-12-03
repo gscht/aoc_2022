0219b85af5d0dab516cb7a90eb048628d2b8218a
gscht <gscht@mailbox.org>
2022-12-03 20:26:14 +0100

    Part 1 complete.

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashSet;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let contents =
+        fs::read_to_string("./input.txt").expect("Should have been able to read the file");
+
+    let lines: Vec<&str> = contents.split('\n').collect();
+    let mut sum = 0;
+    for line in &lines {
+        if line == &"" {
+            continue;
+        }
+        let number_of_items = line.len() / 2;
+        let (compartment_1, compartment_2) = line.split_at(number_of_items);
+
+        let compartment_1: HashSet<char> = compartment_1.chars().collect();
+        let compartment_2: HashSet<char> = compartment_2.chars().collect();
+
+        let intersection: HashSet<_> = compartment_1.intersection(&compartment_2).collect();
+        let found = intersection.iter().next().unwrap();
+        if found.is_uppercase() {
+            let upper_case_letter_number = **found as u32;
+            sum += upper_case_letter_number - 38;
+        } else {
+            let lower_case_letter_number = **found as u32;
+            sum += lower_case_letter_number - 96;
+        }
+    }
+    println!("{sum}");
 }
