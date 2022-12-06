14477fd8d616b5e479693837c4afb82352d9b6e9
gscht <gscht@mailbox.org>
2022-12-06 12:12:52 +0100

    Part 1 done.

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashSet;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let contents =
+        fs::read_to_string("./input.txt").expect("Should have been able to read the file");
+    let lines: Vec<&str> = contents.split('\n').filter(|line| line.len() > 0).collect();
+    let &line = lines.get(0).unwrap();
+    let mut index = 0;
+    while index < &line.len() - 5 {
+        let marker = &line[index..index + 4];
+        println!("{marker}");
+        let chars: HashSet<char> = marker.chars().collect();
+        if chars.len() == 4 {
+            break;
+        }
+        index += 1;
+    }
+    println!("{}", index + 4) ;
 }
