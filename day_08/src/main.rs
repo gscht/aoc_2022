241176a84a448e8469916115455ec33a46278568
gscht <gscht@mailbox.org>
2022-12-08 18:42:42 +0100

    Part 2 done.

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -63,4 +63,63 @@ fn main() {
     let total_visible = 98 * 4 + visible_trees;
 
     println!("Trees visible: {total_visible}");
+    // Part 2
+    let mut max_view = 0;
+    for row in 1..=97 {
+        for col in 1..=97 {
+            let current_tree_height = trees[(row, col)];
+            let mut counter = 0;
+
+            // look up
+            for up in (0..=row-1).rev() {
+                counter += 1;
+                if trees[(up, col)] >= current_tree_height {
+                    break;
+                }
+            }
+            let view_top = counter;
+
+            counter = 0;
+            // look down
+            for down in row + 1..=98 {
+                counter += 1;
+                if trees[(down, col)] >= current_tree_height {
+                    break;
+                }
+            }
+            let view_bottom = counter;
+
+            counter = 0;
+            // look left
+            for left in (0..=col-1).rev() {
+                counter += 1;
+                if trees[(row, left)] >= current_tree_height {
+                    break;
+                }
+            }
+            let view_left = counter;
+
+            counter = 0;
+            // look right
+            for right in col + 1..=98 {
+                counter += 1;
+                if trees[(row, right)] >= current_tree_height {
+                    break;
+                }
+            }
+            let view_right = counter;
+
+            let current_view = view_top * view_bottom * view_left * view_right;
+            if current_view > max_view {
+                println!(
+                    "To top {}, to bottom {}, to left {}, to right {}",
+                    view_top, view_bottom, view_left, view_right
+                );
+                println!("Tree coordinates: {}/{}", row+1, col+1);
+                max_view = current_view;
+                println!("New max found: {max_view}");
+            }
+        }
+    }
+    println!("Max view is {max_view}");
 }
