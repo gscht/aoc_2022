d65cae2156c064971480e9c38e5780a929950383
gscht <gscht@mailbox.org>
2022-12-10 16:50:07 +0100

    Part 1

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
     let lines: Vec<&str> = contents.split('\n').filter(|line| line.len() > 0).collect();
 
-    let mut ticks = 1;
+    let mut ticks = 0;
     let mut x = 1;
     let mut signal = 0;
     for line in lines {
@@ -17,34 +17,37 @@ fn main() {
                 || ticks == 180
                 || ticks == 220
             {
+                println!("1, {} cycle, x={}", ticks, x);
                 signal += x * ticks;
             }
             ticks += 1;
         } else if line.starts_with("addx ") {
+            if ticks == 20
+                || ticks == 60
+                || ticks == 100
+                || ticks == 140
+                || ticks == 180
+                || ticks == 220
+            {
+                println!("2, {} cycle, x={}", ticks, x);
+                signal += x * ticks;
+            }
+            ticks += 1;
+            if ticks == 20
+                || ticks == 60
+                || ticks == 100
+                || ticks == 140
+                || ticks == 180
+                || ticks == 220
+            {
+                println!("3, {} cycle, x={}", ticks, x);
+                signal += x * ticks;
+            }
+            ticks += 1;
             let num: i32 = match line[5..].parse() {
                 Ok(n) => n,
                 Err(_) => panic!("Cannot read"),
             };
-            ticks += 1;
-            if ticks == 20
-                || ticks == 60
-                || ticks == 100
-                || ticks == 140
-                || ticks == 180
-                || ticks == 220
-            {
-                signal += x * ticks;
-            }
-            ticks += 1;
-            if ticks == 20
-                || ticks == 60
-                || ticks == 100
-                || ticks == 140
-                || ticks == 180
-                || ticks == 220
-            {
-                signal += x * ticks;
-            }
             x += num;
         }
     }
