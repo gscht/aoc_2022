9c43fdfeec3fe37135c8d43401527fd113239232
gscht <gscht@mailbox.org>
2022-12-15 09:31:56 +0100

    Add day_15.

---
+++ day_15/src/main/java/dev/gscht/aoc/day_15/Day15.java
@@ -0,0 +1,13 @@
+package dev.gscht.aoc.day_15;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+class Day15 implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+    
+  }
+}
---
+++ day_15/src/test/java/dev/gscht/aoc/day_15/Day15ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gscht.aoc.day_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
