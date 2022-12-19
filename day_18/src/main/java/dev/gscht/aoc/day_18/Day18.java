e5a990aee9279209a5616fbaa07580a2242c5945
gscht <gscht@mailbox.org>
2022-12-19 11:59:57 +0100

    Polish.

--- day_18/src/main/java/dev/gscht/aoc/day_18/Day18.java
+++ day_18/src/main/java/dev/gscht/aoc/day_18/Day18.java
@@ -67,9 +67,8 @@ class Day18 implements ApplicationRunner {
     System.out.println("There are %d sides visible".formatted(totalSidesVisible));
 
     var visible = 0;
-var numberOfPoints = points.size();
+    var numberOfPoints = points.size();
     for (var p : points) {
-      System.out.println("Checking point %s".formatted(p));
       var check = new Point(p).left();
       var result = expose(check);
       pointsCache.put(check, result);
@@ -136,12 +135,7 @@ var numberOfPoints = points.size();
       if (points.contains(pop)) {
         continue;
       }
-      if (x <= minX
-          || x >= maxX
-          || y <= minY
-          || y >= maxY
-          || z <= minZ
-          || z >= maxZ) {
+      if (x <= minX || x >= maxX || y <= minY || y >= maxY || z <= minZ || z >= maxZ) {
         return true;
       }
       if (seen.contains(pop.toString())) {
@@ -155,90 +149,6 @@ var numberOfPoints = points.size();
       stack.push(new Point(pop).backwards());
       stack.push(new Point(pop).forward());
     }
-    return false;
-  }
-
-  private boolean canReachOutside(Point p) {
-    if (points.contains(p)) {
-      return false;
-    }
-    var seen = new HashSet<Point>();
-    var stack = new Stack<Point>();
-    stack.push(p);
-    while (!stack.isEmpty()) {
-      var pop = stack.pop();
-      if (points.contains(pop)) {
-        continue;
-      }
-      if (!(0 <= pop.x && pop.x <= 19)) {
-        return true;
-      }
-      if (!(0 <= pop.y && pop.y <= 19)) {
-        return true;
-      }
-      if (!(0 <= pop.z && pop.z <= 19)) {
-        return true;
-      }
-      if (seen.contains(pop)) {
-        continue;
-      }
-      seen.add(pop);
-
-      stack.push(new Point(p.x - 1, p.y, p.z));
-      stack.push(new Point(p.x + 1, p.y, p.z));
-      stack.push(new Point(p.x, p.y - 1, p.z));
-      stack.push(new Point(p.x, p.y + 1, p.z));
-      stack.push(new Point(p.x, p.y, p.z - 1));
-      stack.push(new Point(p.x, p.y, p.z + 1));
-    }
-    return false;
-  }
-
-  private boolean reachesRock(Point p, int direction) {
-    if (direction == 1) {
-      for (var x = p.x - 1; x >= 0; x--) {
-        var n = new Point(x, p.y, p.z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    } else if (direction == 2) {
-      for (var x = p.x + 1; x <= maxX; x++) {
-        var n = new Point(x, p.y, p.z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    } else if (direction == 3) {
-      for (var y = p.y - 1; y >= 0; y--) {
-        var n = new Point(p.x, y, p.z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    } else if (direction == 4) {
-      for (var y = p.y + 1; y <= maxY; y++) {
-        var n = new Point(p.x, y, p.z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    } else if (direction == 5) {
-      for (var z = p.z - 1; z >= 0; z--) {
-        var n = new Point(p.x, p.y, z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    } else if (direction == 6) {
-      for (var z = p.z + 1; z <= maxZ; z++) {
-        var n = new Point(p.x, p.y, z);
-        if (points.contains(n)) {
-          return true;
-        }
-      }
-    }
-
     return false;
   }
 }
